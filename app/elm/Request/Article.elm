a2d41ff52410bd0d12668fdc63323f9f78f592a6
breezyboa <blitzritz@gmail.com>
2018-05-18 22:43:18 +0530

    Changed mock calls to Https

--- app/elm/Request/Article.elm
+++ app/elm/Request/Article.elm
@@ -5,9 +5,9 @@ import Data.Article exposing (Article, ArticleShort, decodeArticles, decodeArtic
 
 requestArticles : Http.Request (List ArticleShort)
 requestArticles =
-    Http.get "http://www.mocky.io/v2/5afd46c63200005f00f1ab39" decodeArticles
+    Http.get "https://www.mocky.io/v2/5afd46c63200005f00f1ab39" decodeArticles
 
 
 requestArticle : Int -> Http.Request Article
 requestArticle aId =
-    Http.get "http://www.mocky.io/v2/5afd46363200005300f1ab36" decodeArticle
+    Http.get "https://www.mocky.io/v2/5afd46363200005300f1ab36" decodeArticle
