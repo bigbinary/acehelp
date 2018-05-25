abb6309b9f240971fe497976657f2ecc8b429d0d
piyushtiw <piyush@bigbinary.com>
2018-05-26 02:04:09 +0530

    article and url create api change

--- app/controllers/article_controller.rb
+++ app/controllers/article_controller.rb
@@ -1,6 +1,6 @@
 class ArticleController < ApplicationController
 
-  before_action :set_article, only: [:show, :update, :destroy]
+  before_action :load_article, only: [:show, :update, :destroy]
   
   def index
     article_scope = if params[:url].present?
@@ -17,34 +17,32 @@ class ArticleController < ApplicationController
   end
 
   def create
-    article = Article.new(article_params)
-    article[:organization_id] = @organization.id
-
-    if article.save
-      render json: {message: "Article created successfully"}, status: 200
-    else
-      render_bad_request article.errors.full_messages.join(',')
-    end
+    Article.create!(article_creation_params)
+    render json: { message: "Article created successfully" }, status: 200
   end
 
   def update
     @article.update!(article_params)
-    render json: {message: "Article updated successfully"}, status: 200
+    render json: { message: "Article updated successfully" }, status: 200
   end
 
   def destroy
     @article.destroy!
-    render json: {message: "Article deleted successfully"}, status: 200
+    render json: { message: "Article deleted successfully" }, status: 200
   end
 
   private
 
-  def set_article
+  def load_article
     @article = Article.find_by!(id: params[:id], organization_id: @organization.id)
   end
 
   def article_params
     params.require(:article).permit(:title, :desc, :category_id)
+  end
+
+  def article_creation_params
+    article_params.merge!(organization_id: @organization.id)
   end
 
 end
--- app/controllers/url_controller.rb
+++ app/controllers/url_controller.rb
@@ -1,6 +1,6 @@
 class UrlController < ApplicationController
 
-  before_action :set_url, only: [:update, :destroy]
+  before_action :load_url, only: [:update, :destroy]
   
   def index
     urls = @organization.urls
@@ -8,29 +8,23 @@ class UrlController < ApplicationController
   end
 
   def create
-    url = Url.new(url_params)
-    url[:organization_id] = @organization.id
-
-    if url.save
-      render json: {message: "Url created successfully"}, status: 200
-    else
-      render_bad_request url.errors.full_messages.join(',')
-    end
+    Url.create!(url_creation_params)
+    render json: { message: "Url created successfully" }, status: 200
   end
 
   def update
     @url.update!(url_params)
-    render json: {message: "url updated successfully"}, status: 200
+    render json: { message: "url updated successfully" }, status: 200
   end
 
   def destroy
     @url.destroy!
-    render json: {message: "url deleted successfully"}, status: 200
+    render json: { message: "url deleted successfully" }, status: 200
   end
 
   private
 
-  def set_url
+  def load_url
     @url = Url.find_by!(id: params[:id], organization_id: @organization.id)
   end
 
@@ -38,4 +32,7 @@ class UrlController < ApplicationController
     params.require(:url).permit(:url)
   end
 
+  def url_creation_params
+    url_params.merge!(organization_id: @organization.id)
+  end
 end
