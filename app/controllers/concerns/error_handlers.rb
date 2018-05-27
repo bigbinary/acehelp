77d5269fe15df495b7a3b53b13885dc59d7f59ee
piyushtiw <piyush@bigbinary.com>
2018-05-27 16:19:50 +0530

    error msg change

--- app/controllers/concerns/error_handlers.rb
+++ app/controllers/concerns/error_handlers.rb
@@ -2,21 +2,10 @@ module Concerns::ErrorHandlers
   extend ::ActiveSupport::Concern
 
   def render_unauthorized(err)
-    render json: json_body(err), status: :unauthorized
+    render json: { errors: err }, status: :unauthorized
   end
 
   def render_bad_request(err)
-    render json: json_body(err), status: :bad_request
-  end
-
-  private
-
-  #
-  # Create json meta body for given message
-  # @param message [String] Error Message
-  #
-  # @return [Hash] Meta Body
-  def json_body(message)
-    { errors: message }
+    render json: { errors: err }, status: :bad_request
   end
 end
