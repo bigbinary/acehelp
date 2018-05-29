28984d65a712c7479077c1f8e82df2bbcd16f210
piyushtiw <piyush@bigbinary.com>
2018-05-29 19:54:38 +0530

    rescue_from ActiveRecord::RecordNotFound

--- app/controllers/api/v1/base_controller.rb
+++ app/controllers/api/v1/base_controller.rb
@@ -5,15 +5,7 @@ module Api
     class BaseController < ActionController::Base
       include ::Concerns::ErrorHandlers
 
-      rescue_from Exception, with: :handle_api_exceptions
-
-      private
-
-        def handle_api_exceptions(exception)
-          if (exception.class.name == "ActiveRecord::RecordNotFound")
-            render_not_found "Record not found"
-          end
-        end
+      rescue_from ActiveRecord::RecordNotFound, with: :render_not_found
     end
   end
 end
