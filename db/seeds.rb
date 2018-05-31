ec6329abd626d11c2562b0214cc05ff5e103c680
Piyush Tiwari <piyush@bigbinary.com>
2018-05-31 21:46:30 +0530

    seed data for all tables (#55)

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,40 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+org1 = Organization.create! name: "ace-invoice"
+org2 = Organization.create! name: "healthcity"
+
+c1 = Category.create! name: "Getting Started"
+a1 = c1.articles.create! title: "How do I put JavaScript code in my website?", desc: "coming soon", organization_id: org1.id
+a2 = c1.articles.create! title: "Will putting JavaScript code in my website will make my site slower?", desc: "coming soon", organization_id: org2.id
+
+c2 = Category.create! name: "Integrations"
+a3 = c2.articles.create! title: "Do you provide integration with wordpress?", desc: "coming soon", organization_id: org1.id
+a4 = c2.articles.create! title: "Do you provide integration for PHP applications?", desc: "coming soon", organization_id: org2.id
+
+c3 = Category.create! name: "Pricing"
+a5 = c3.articles.create! title: "Do I need to put credit card to try it out?", desc: "coming soon", organization_id: org1.id
+a6 = c3.articles.create! title: "Do you offer custom plan?", desc: "coming soon", organization_id: org2.id
+a7 = c3.articles.create! title: "Do you offer discount on yearly plan?", desc: "coming soon", organization_id: org1.id
+
+u1 = org1.urls.create! url: "http://ace-invoice.com/getting-started"
+u2 = org1.urls.create! url: "http://ace-invoice.com/integrations"
+u3 = org1.urls.create! url: "http://ace-invoice.com/pricing"
+
+u5 = org2.urls.create! url: "http://healthcity.com/getting-started"
+u6 = org2.urls.create! url: "http://healthcity.com/integrations"
+u7 = org2.urls.create! url: "http://healthcity.com/pricing"
+
+ArticleUrl.create! article_id: a1.id, url_id: u1.id
+ArticleUrl.create! article_id: a3.id, url_id: u1.id
+ArticleUrl.create! article_id: a1.id, url_id: u3.id
+ArticleUrl.create! article_id: a3.id, url_id: u2.id
+ArticleUrl.create! article_id: a5.id, url_id: u2.id
+ArticleUrl.create! article_id: a7.id, url_id: u1.id
+
+ArticleUrl.create! article_id: a2.id, url_id: u5.id
+ArticleUrl.create! article_id: a4.id, url_id: u5.id
+ArticleUrl.create! article_id: a6.id, url_id: u6.id
+ArticleUrl.create! article_id: a2.id, url_id: u7.id
+ArticleUrl.create! article_id: a4.id, url_id: u6.id
+ArticleUrl.create! article_id: a6.id, url_id: u7.id
--- lib/tasks/setup.rake
+++ lib/tasks/setup.rake
@@ -37,19 +37,6 @@ end
 desc "Deletes all records and populates sample data"
 task setup_sample_data: [:environment] do
   delete_all_records_from_all_tables
-
-  c1 = Category.create! name: "Getting Started"
-  c1.articles.create! title: "How do I put JavaScript code in my website?", desc: "coming soon"
-  c1.articles.create! title: "Will putting JavaScript code in my website will make my site slower?", desc: "coming soon"
-
-  c2 = Category.create! name: "Integrations"
-  c2.articles.create! title: "Do you provide integration with wordpress?", desc: "coming soon"
-  c2.articles.create! title: "Do you provide integration for PHP applications?", desc: "coming soon"
-
-  c3 = Category.create! name: "Pricing"
-  c3.articles.create! title: "Do I need to put credit card to try it out?", desc: "coming soon"
-  c3.articles.create! title: "Do you offer custom plan?", desc: "coming soon"
-  c3.articles.create! title: "Do you offer discount on yearly plan?", desc: "coming soon"
-
+  system "rake db:seed"
   puts "sample data was added successfully"
 end
