f0eb67740577a39b1aa8dc41051a37485169cdf5
piyushtiw <piyush@bigbinary.com>
2018-06-01 17:07:02 +0530

    fix indentation

--- app/controllers/api/v1/contacts_controller.rb
+++ app/controllers/api/v1/contacts_controller.rb
@@ -2,22 +2,22 @@
 
 module Api
   module V1
-		class ContactsController < BaseController
-		  def create
-		    contact = Contact.new(contact_params)
+    class ContactsController < BaseController
+      def create
+        contact = Contact.new(contact_params)
 
-		    if contact.save
-		      render json: { message: "Thank you for your message. We will contact you soon!"}
-		    else
-		      render_unprocessable_entity contact.errors.full_messages.to_sentence
-		    end
-		  end
+        if contact.save
+          render json: { message: "Thank you for your message. We will contact you soon!"}
+        else
+          render_unprocessable_entity contact.errors.full_messages.to_sentence
+        end
+      end
 
-		  private
+      private
 
-		    def contact_params
-		      params.require(:contact).permit(:name, :email, :message)
-		    end
-		end
+        def contact_params
+          params.require(:contact).permit(:name, :email, :message)
+        end
+    end
   end
 end
