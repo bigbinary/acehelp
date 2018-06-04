21efd2ed47d41f4b5d1201e0223dfc9d1260c109
piyushtiw <piyush@bigbinary.com>
2018-06-04 14:43:23 +0530

    regex change

--- config/application.rb
+++ config/application.rb
@@ -16,7 +16,7 @@ module Acehelp
     # enable cors
     config.middleware.insert_before 0, Rack::Cors do
       allow do
-        origins '.*\.(heroku|acehelp)\.com'
+        origins '*\.(heroku|acehelp)\.com'
         resource '*', headers: :any, methods: [:get, :post, :put, :delete, :options]
       end
     end
