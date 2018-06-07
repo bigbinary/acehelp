4ac39093f33e2ddc35257275dddc1d2be9e66e8a
sudeeptarlekar <sudeeptarlekar@gmail.com>
2018-06-08 00:18:23 +0530

    Resolved PR comments.

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,6 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  def draw(routes_name)
-    instance_eval(File.read(Rails.root.join('config', 'routes', "#{routes_name}.rb")))
-  end
-
-  draw :admin
-
   devise_for :users
 
   post "/graphql", to: "graphql#execute"
--- config/routes/admin.rb
+++
@@ -1,5 +0,0 @@
-namespace :admin do
-  resources :dashboard, only: [:index]
-  resources :articles
-  resources :urls
-end
