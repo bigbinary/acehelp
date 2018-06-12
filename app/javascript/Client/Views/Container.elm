f6874ed9ae9a2a301db6482ec5946685439018f0
breezyboa <blitzritz@gmail.com>
2018-06-13 00:02:40 +0530

    Clean up container styles

--- app/javascript/Client/Views/Container.elm
+++ app/javascript/Client/Views/Container.elm
@@ -1,7 +1,7 @@
 module Views.Container exposing (..)
 
 import Html exposing (..)
-import Html.Attributes exposing (style, class, classList)
+import Html.Attributes exposing (id, style, class, classList)
 import Html.Events exposing (onClick)
 import Svg
 import Svg.Attributes
@@ -21,22 +21,13 @@ rowView customStyle additionalClasses children =
 leftArrowButton : msg -> Html msg
 leftArrowButton clickMsg =
     div
-        [ class "arrow left"
-        , style
-            [ ( "position", "absolute" )
-            , ( "top", "0" )
-            , ( "bottom", "0" )
-            , ( "left", "15px" )
-            , ( "line-height", "0" )
-            , ( "cursor", "pointer" )
-            ]
+        [ class "arrow-left"
         , onClick <| clickMsg
         ]
         [ Svg.svg
             [ Svg.Attributes.width "50"
             , Svg.Attributes.height "80"
             , Svg.Attributes.viewBox "0 0 50 80"
-            , Svg.Attributes.style "width: 20px; height: 20px; fill: #fff; position: absolute; top: 50%; transform: translateY(-50%);"
             ]
             [ Svg.polyline
                 [ Svg.Attributes.fill "none"
@@ -54,23 +45,13 @@ leftArrowButton clickMsg =
 closeButton : msg -> Html msg
 closeButton clkMsg =
     div
-        [ style
-            [ ( "position", "absolute" )
-            , ( "top", "0" )
-            , ( "bottom", "0" )
-            , ( "right", "35px" )
-            , ( "line-height", "0" )
-            , ( "cursor", "pointer" )
-            ]
+        [ class "close-button"
         , onClick <| clkMsg
         ]
         [ Svg.svg
             [ Svg.Attributes.width "60"
             , Svg.Attributes.height "60"
             , Svg.Attributes.viewBox "0 0 60 60"
-
-            -- This is so badly inconsistent with Html style
-            , Svg.Attributes.style "width: 20px; height: 20px; fill: #fff; position: absolute; top: 50%; transform: translateY(-50%);"
             ]
             [ Svg.path
                 [ Svg.Attributes.d "M35.7,30L58.8,6.8C59.6,6.1,60,5.1,60,4c0-2.2-1.8-4-4-4c-1.1,0-2.1,0.4-2.8,1.2L30,24.3L6.8,1.2C6.1,0.4,5.1,0,4,0C1.8,0,0,1.8,0,4c0,1.1,0.4,2.1,1.2,2.8L24.3,30L1.2,53.2C0.4,53.9,0,54.9,0,56c0,2.2,1.8,4,4,4c1.1,0,2.1-0.4,2.8-1.2L30,35.7l23.2,23.2c0.7,0.7,1.7,1.2,2.8,1.2c2.2,0,4-1.8,4-4c0-1.1-0.4-2.1-1.2-2.8L35.7,30z" ]
@@ -84,11 +65,7 @@ topBar showBack onBack onClose =
     let
         initialChildren =
             [ span
-                [ style
-                    [ ( "text-align", "center" )
-                    , ( "display", "block" )
-                    , ( "color", "#fff" )
-                    ]
+                [ id "top-bar-title"
                 ]
                 [ text "Ace Help" ]
             , closeButton onClose
