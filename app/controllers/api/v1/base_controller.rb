41bb75f6ad6cec80464254699e8dbce1679a8d38
breezyboa <blitzritz@gmail.com>
2018-06-23 11:10:57 +0530

    Revert base controller changes

--- app/controllers/api/v1/base_controller.rb
+++ app/controllers/api/v1/base_controller.rb
@@ -6,7 +6,6 @@ module Api
       include ::Concerns::ErrorHandlers
 
       rescue_from ActiveRecord::RecordNotFound, with: :render_not_found
-      skip_before_action :verify_authenticity_token
     end
   end
 end
