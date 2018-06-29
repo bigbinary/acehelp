8212951cbcc38fb6a31749c212d1d8c9214cda2d
Abdul Shajin <shajinvk@gmail.com>
2018-06-29 20:33:08 +0530

    Review changes - 2 Unnecessary Code got added while resolving conflict with master branch Rubocop suggested changes

--- app/controllers/concerns/load_organization.rb
+++ app/controllers/concerns/load_organization.rb
@@ -20,27 +20,26 @@ module LoadOrganization
     render_unauthorized_request(err: :missing_keys) && return if api_key.blank?
     @organization = Organization.find_by(api_key: api_key)
     render_unauthorized_request && return if @organization.blank?
-    
-    @organization.inspect
   end
 
   private
-  def graphql_api_call?
-    params[:controller] == 'graphql'
-  end
 
-  def render_unauthorized_request(err: :invalid_keys)
-    err_text = "UNAUTHORIZED REQUEST: #{err.to_s.titleize}"
-    if graphql_api_call?
-      render_unauthorized_org_error_graphql(err_text)
-    else
-      render_unauthorized(err_text)
+    def graphql_api_call?
+      params[:controller] == "graphql"
     end
-  end
 
-  def render_unauthorized_org_error_graphql(message)
-    render json: Utils::ErrorHandler.new.generate_graphql_error_with_root(message,
-                                                         path: 'load_organization',
-                                                         extensions: {code: 'UNAUTHORIZED'})
-  end
+    def render_unauthorized_request(err: :invalid_keys)
+      err_text = "UNAUTHORIZED REQUEST: #{err.to_s.titleize}"
+      if graphql_api_call?
+        render_unauthorized_org_error_graphql(err_text)
+      else
+        render_unauthorized(err_text)
+      end
+    end
+
+    def render_unauthorized_org_error_graphql(message)
+      render json: Utils::ErrorHandler.new.generate_graphql_error_with_root(message,
+                                                           path: "load_organization",
+                                                           extensions: { code: "UNAUTHORIZED" })
+    end
 end
