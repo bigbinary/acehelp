da6b263ff5ad562249426cda60fad2a90fcd975c
Vipul A M <vipulnsward@gmail.com>
2018-07-01 20:40:35 +0530

    Cleanups

--- app/graphql/utils/error_handler.rb
+++ app/graphql/utils/error_handler.rb
@@ -1,22 +1,19 @@
-module Utils
-  class ErrorHandler
-
-    def generate_graphql_error_with_root(message, path:, extensions: {})
-      path = [path] if path.is_a?(String)
-      {
+class Utils::ErrorHandler
+  def generate_graphql_error_with_root(message, path:, extensions: {})
+    path = [path] if path.is_a?(String)
+    {
         data: nil,
         errors: [
-          {
-            message: message,
-            path: path,
-            extensions: extensions
-          }
+            {
+                message: message,
+                path: path,
+                extensions: extensions
+            }
         ]
-      }
-    end
+    }
+  end
 
-    def object_error_full_messages(obj)
-      "Invalid Input: #{obj.errors.full_messages.join(', ')}"
-    end
+  def object_error_full_messages(obj)
+    "Invalid Input: #{obj.errors.full_messages.join(', ')}"
   end
 end
