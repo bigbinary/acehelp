0d284f4ba4ac93bfc3fdbd3d63eda9281d379d79
sudeeptarlekar <sudeeptarlekar@gmail.com>
2018-07-16 14:38:36 +0530

    Added url as optional parameter in graphql query.

--- app/graphql/resolvers/articles_search.rb
+++ app/graphql/resolvers/articles_search.rb
@@ -4,10 +4,17 @@ class Resolvers::ArticlesSearch < GraphQL::Function
   type !types[Types::ArticleType]
 
   argument :id, types.String
+  argument :url, types.String
 
   def call(obj, args, context)
     if args[:id].present?
-      Article.where(id: args[:id]).for_organization(context[:organization])
+      if args[:url].present?
+        Url.find_by(url: args[:url]).articles.where(id: args[:id]).for_organization(context[:organization])
+      else
+        Article.where(id: args[:id]).for_organization(context[:organization])
+      end
+    elsif args[:url].present?
+      Url.find_by(url: args[:url]).articles.for_organization(context[:organization])
     else
       Article.for_organization(context[:organization])
     end
