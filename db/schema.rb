5fe87ee7fde5433a6272f04e47ad333cb87fbeab
Tejaswini <tejaswini776@gmail.com>
2018-07-19 12:32:07 +0530

    Add foreign keys back to tables

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_07_12_052529) do
+ActiveRecord::Schema.define(version: 2018_07_19_065948) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "pgcrypto"
@@ -86,4 +86,7 @@ ActiveRecord::Schema.define(version: 2018_07_12_052529) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "articles", "organizations"
+  add_foreign_key "urls", "organizations"
+  add_foreign_key "users", "organizations"
 end
