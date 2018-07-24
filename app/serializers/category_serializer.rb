a021ffde4f96b7a2e79d7427c9ed496079b4780c
Vipul A M <vipulnsward@gmail.com>
2018-07-25 00:13:40 +0530

    rm serializers

--- app/serializers/category_serializer.rb
+++
@@ -1,7 +0,0 @@
-# frozen_string_literal: true
-
-class CategorySerializer < ActiveModel::Serializer
-  attributes :id, :name
-
-  has_many :articles, only: %i(id title)
-end
--- app/serializers/url_serializer.rb
+++
@@ -1,5 +0,0 @@
-# frozen_string_literal: true
-
-class UrlSerializer < ActiveModel::Serializer
-  attributes :id, :url
-end
