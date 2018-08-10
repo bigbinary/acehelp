99e1145e4383439d1d8d8476f83861cbf8ad82ad
Tejaswini <tejaswini776@gmail.com>
2018-08-10 12:47:42 +0530

    fixed test cases for article urls

--- test/models/article_url_test.rb
+++ test/models/article_url_test.rb
@@ -10,8 +10,15 @@ class ArticleUrlTest < ActiveSupport::TestCase
 
   test "add url for article" do
     article_url_count = ArticleUrl.count
+    @article.urls.destroy_all
     @article.urls << @url
 
-    assert_equal ArticleUrl.count, article_url_count + 1
+    assert_equal ArticleUrl.count, article_url_count
+  end
+
+  test "add same url to throw error" do
+    assert_raises ActiveRecord::RecordInvalid do
+      @article.urls << @url
+    end
   end
 end
