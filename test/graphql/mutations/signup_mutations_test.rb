f78219d63abad2bb4a1d0a6e50e9ae87cab6dd16
sudeeptarlekar <sudeeptarlekar@gmail.com>
2018-08-14 11:32:06 +0530

    Added test cases.

---
+++ test/graphql/mutations/signup_mutations_test.rb
@@ -0,0 +1,57 @@
+# frozen_string_literal: true
+
+require "test_helper"
+require "graphql/client_host"
+
+class Mutations::SignupMutationsTest < ActiveSupport::TestCase
+  setup do
+    @signup_mutation = <<-'GRAPHQL'
+            mutation($input: SignupInput!) {
+              signup(input: $input) {
+                user {
+                  id,
+                  email
+                }
+                errors {
+                  message
+                  path
+                }
+              }
+            }
+    GRAPHQL
+  end
+
+  test "signup mutations" do
+    result = AceHelp::Client.execute(@signup_mutation, input: {
+      first_name: "morgan",
+      email: "morgan@example.com",
+      password: "welcome",
+      confirm_password: "welcome"
+    })
+    assert_equal result.data.signup.user.email, "morgan@example.com"
+    assert_nil result.data.signup.errors
+  end
+
+  test "signup when user is already present" do
+    user = users :hunt
+    result = AceHelp::Client.execute(@signup_mutation, input: {
+      first_name: user.first_name,
+      email: user.email,
+      password: "welcome",
+      confirm_password: "welcome"
+    })
+    assert_nil result.data.signup.user
+    assert_not_empty result.data.signup.errors.flat_map(&:path) & ["signup", "email"]
+  end
+
+  test "signup when passwords do no match" do
+    result = AceHelp::Client.execute(@signup_mutation, input: {
+      first_name: "drake",
+      email: "drake@example.com",
+      password: "welcome",
+      confirm_password: "wrlcome"
+    })
+    assert_nil result.data.signup.user
+    assert_not_empty result.data.signup.errors.flat_map(&:path) & ["signup", "password"]
+  end
+end
