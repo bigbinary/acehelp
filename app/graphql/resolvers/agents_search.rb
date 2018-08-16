d4b180c9ab1114e47cc3af309c330382cd57a7de
Tejaswini <tejaswini776@gmail.com>
2018-08-16 16:00:55 +0530

    added quesry type for agents 1. added query search 2. declared in query_type which will fetch agents according organization

---
+++ app/graphql/resolvers/agents_search.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+class Resolvers::AgentsSearch < GraphQL::Function
+  type !types[Types::UserType]
+
+  def call(obj, args, context)
+    query = User.for_organization(
+      context[:organization]
+    )
+    query
+  end
+end
