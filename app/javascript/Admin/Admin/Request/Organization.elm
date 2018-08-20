9daad192476d903ca5ae4d775e60205a40106391
sudeeptarlekar <sudeeptarlekar@gmail.com>
2018-08-20 21:18:06 +0530

    Removed unwanted code from organization module

--- app/javascript/Admin/Admin/Request/Organization.elm
+++ app/javascript/Admin/Admin/Request/Organization.elm
@@ -11,37 +11,6 @@ import GraphQL.Client.Http as GQLClient
 import GraphQL.Request.Builder as GQLBuilder
 
 
-organizationUrl : NodeEnv -> OrganizationId -> Url
-organizationUrl env organizationId =
-    (baseUrl env) ++ "/api/v1/organization/" ++ toString (organizationId) ++ "/data"
-
-
-requestOrganization : NodeEnv -> ApiKey -> OrganizationId -> Http.Request OrganizationResponse
-requestOrganization env apiKey organizationId =
-    let
-        url =
-            (organizationUrl env organizationId)
-
-        headers =
-            List.concat
-                [ defaultRequestHeaders
-                , [ (Http.header "api-key" apiKey) ]
-                ]
-
-        decoder =
-            field "_id" JD.string
-    in
-        Http.request
-            { method = "GET"
-            , headers = headers
-            , url = url
-            , body = Http.emptyBody
-            , expect = Http.expectJson organization
-            , timeout = Nothing
-            , withCredentials = False
-            }
-
-
 requestCreateOrganization : OrganizationData -> Reader ( NodeEnv, AppUrl ) (Task GQLClient.Error Organization)
 requestCreateOrganization orgInputs =
     Reader.Reader
