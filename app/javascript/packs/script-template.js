0f95bd1d09554d584c9f8498bbdb5870142feae9
Tejaswini <tejaswini776@gmail.com>
2018-08-30 16:27:23 +0530

    Set user by token 1. find user by passed token 2. in token we have uid set as user email 3. validated token for the found user else render unauthorized

--- app/javascript/packs/script-template.js
+++ app/javascript/packs/script-template.js
@@ -1,6 +1,6 @@
 var req = new XMLHttpRequest();
 var baseUrl = "";
-var apiKey = "6aa94783daeccbea5c63";
+var apiKey = "YOURAPIKEY";
 
 var script = document.createElement("script");
 script.type = "text/javascript";
