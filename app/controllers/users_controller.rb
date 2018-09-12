b728b4ef1d7b43e05ec7dfb520d875097f30d8d5
Tejaswini <tejaswini776@gmail.com>
2018-09-12 17:24:15 +0530

    updated user sign_in redirection according to new warden changes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,8 +2,8 @@
 
 class UsersController < ApplicationController
   def sign_in
-    if session[:uid].present?
-      @resource = User.find_by(email: session["uid"])
+    if warden.user.present?
+      @resource = User.find_by!(email: warden.user["email"])
       organization = @resource.organizations.first
       redirect_to(organization_articles_path(organization.api_key)) && (return)
     end
