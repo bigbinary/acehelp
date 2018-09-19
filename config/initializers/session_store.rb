886b667b6b909b894e4a6650c36e8e9bf38beaa8
Prathamesh Sonpatki <csonpatki@gmail.com>
2018-09-19 17:34:13 +0530

    Change key for session store

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,1 +1,1 @@
-Rails.application.config.session_store :cookie_store, :key => '_acehelp_session', expire_after: 30.minutes
+Rails.application.config.session_store :cookie_store, :key => '__acehelp_session', expire_after: 30.minutes
