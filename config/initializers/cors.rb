3de120263fe57e4fb241edb1197286eba3c3e155
Prathamesh Sonpatki <csonpatki@gmail.com>
2018-09-21 17:59:51 +0530

    Verifying if the issue related to session is due to cors or not

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,45 +1,45 @@
 # frozen_string_literal: true
 
 # enable cors
-Rails.application.config.middleware.insert_before 0, Rack::Cors do
+# Rails.application.config.middleware.insert_before 0, Rack::Cors do
 
 
-  allow do
-    origins '*'
+#   allow do
+#     origins '*'
 
-    resource '*',
-      headers: :any,
-      methods: [:get, :post, :put, :patch, :delete, :options, :head]
-  end
+#     resource '*',
+#       headers: :any,
+#       methods: [:get, :post, :put, :patch, :delete, :options, :head]
+#   end
 
 
-  allow do
-    origins "*"
-    resource "/api/*/all", headers: :any, methods: [:get, :post, :put, :delete, :options, :head]
-  end
+#   allow do
+#     origins "*"
+#     resource "/api/*/all", headers: :any, methods: [:get, :post, :put, :delete, :options, :head]
+#   end
 
-  allow do
-    origins "*"
-    resource "/api/*/article", headers: :any, methods: [:get, :post, :put, :delete, :options, :head]
-  end
+#   allow do
+#     origins "*"
+#     resource "/api/*/article", headers: :any, methods: [:get, :post, :put, :delete, :options, :head]
+#   end
 
-  allow do
-    origins "*"
-    resource "/api/*/article/*", headers: :any, methods: [:get, :post, :put, :delete, :options, :head]
-  end
+#   allow do
+#     origins "*"
+#     resource "/api/*/article/*", headers: :any, methods: [:get, :post, :put, :delete, :options, :head]
+#   end
 
-  allow do
-    origins "*"
-    resource "/api/*/articles/*", headers: :any, methods: [:get, :post, :put, :delete, :options, :head]
-  end
+#   allow do
+#     origins "*"
+#     resource "/api/*/articles/*", headers: :any, methods: [:get, :post, :put, :delete, :options, :head]
+#   end
 
-  allow do
-    origins "*"
-    resource "/packs/*", headers: :any, methods: :get
-  end
+#   allow do
+#     origins "*"
+#     resource "/packs/*", headers: :any, methods: :get
+#   end
 
-  allow do
-    origins "*"
-    resource "/graphql", headers: :any, methods: [:get, :post, :put, :delete, :options, :head]
-  end
-end
+#   # allow do
+#   #   origins "*"
+#   #   resource "/graphql", headers: :any, methods: [:get, :post, :put, :delete, :options, :head]
+#   # end
+# end
