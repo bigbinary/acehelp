935c324e0d75bc37f606599d3fb0d5c20d7c30b2
Tejaswini <tejaswini776@gmail.com>
2018-09-25 15:10:02 +0530

    added test case to check visibility is true by default

--- db/schema.rb
+++ db/schema.rb
@@ -123,7 +123,7 @@ ActiveRecord::Schema.define(version: 2018_09_24_052155) do
 
   create_table "settings", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
     t.string "base_url"
-    t.boolean "visibility", default: false, null: false
+    t.boolean "visibility", default: true, null: false
     t.uuid "organization_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
