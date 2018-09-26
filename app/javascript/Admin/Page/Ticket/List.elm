95a373e7ceb6c4f7e791d954898d7c128c412de9
Vishal Telangre <the@vishaltelangre.com>
2018-09-26 10:04:50 +0530

    Present ticket listing in a tabular view
    Modified the markup in the view function of Page.Ticket.List module from a DIV -> SPAN structure to the TABLE -> TD structure.
    Added markup to display the table with appropriate Bootstrap 4.0 classes to so that rows appear highlighted when mouse is hovered on it.
    Added markup for the heading section of table.
    Added markup for the rows and provisioned it to display "View" link in the last column.

--- app/javascript/Admin/Page/Ticket/List.elm
+++ app/javascript/Admin/Page/Ticket/List.elm
@@ -75,22 +75,38 @@ view orgKey model =
             [ text (renderError model.error) ]
         , div
             [ id "content-wrapper" ]
-            (List.map
-                (\ticket ->
-                    rows orgKey model ticket
-                )
-                model.ticketList
-            )
+            [ table
+                [ class "table table-hover" ]
+                [ thead
+                    []
+                    [ tr
+                        []
+                        [ th [ scope "col" ] [ text "ID" ]
+                        , th [ scope "col" ] [ text "Name" ]
+                        , th [ scope "col" ] [ text "Email" ]
+                        , th [ scope "col" ] [ text "Message" ]
+                        , th [] []
+                        ]
+                    ]
+                , tbody []
+                    (List.map
+                        (\ticket ->
+                            rows orgKey model ticket
+                        )
+                        model.ticketList
+                    )
+                ]
+            ]
         ]
 
 
 rows : ApiKey -> Model -> Ticket -> Html Msg
 rows orgKey model ticket =
-    div
-        [ class "listingRow" ]
-        [ span [ class "row-id" ] [ text ticket.id ]
-        , span [ class "row-name" ] [ text ticket.name ]
-        , span [ class "row-email" ] [ text ticket.email ]
-        , span [ class "row-message" ] [ text ticket.message ]
-        , span [] [ a [ href <| routeToString <| TicketEdit orgKey ticket.id ] [ text " | Edit Ticket" ] ]
+    tr
+        []
+        [ td [ class "row-id" ] [ text ticket.id ]
+        , td [ class "row-name" ] [ text ticket.name ]
+        , td [ class "row-email" ] [ text ticket.email ]
+        , td [ class "row-message" ] [ text ticket.message ]
+        , td [] [ a [ href <| routeToString <| TicketEdit orgKey ticket.id ] [ text "View" ] ]
         ]
