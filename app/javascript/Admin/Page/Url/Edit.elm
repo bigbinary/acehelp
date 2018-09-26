4a48f7cbc972c4eb8aba138a9f9ab46320de8a04
breezyboa <blitzritz@gmail.com>
2018-09-26 16:35:32 +0530

    Auto add base url on url creation

--- app/javascript/Admin/Page/Url/Edit.elm
+++ app/javascript/Admin/Page/Url/Edit.elm
@@ -1,7 +1,9 @@
 module Page.Url.Edit exposing (Model, Msg(..), init, initModel, save, update, urlInputs, view)
 
 import Admin.Data.ReaderCmd exposing (..)
+import Admin.Data.Setting exposing (..)
 import Admin.Data.Url exposing (..)
+import Admin.Request.Setting exposing (..)
 import Admin.Request.Url exposing (..)
 import Admin.Views.Common exposing (errorView)
 import Field exposing (..)
@@ -24,6 +26,7 @@ type alias Model =
     , success : Maybe String
     , url : Field String String
     , urlId : UrlId
+    , baseUrl : Maybe String
     }
 
 
@@ -33,13 +36,16 @@ initModel urlId =
     , success = Nothing
     , url = Field (validateEmpty "Url") ""
     , urlId = urlId
+    , baseUrl = Nothing
     }
 
 
 init : UrlId -> ( Model, List (ReaderCmd Msg) )
 init urlId =
     ( initModel urlId
-    , [ Strict <| Reader.map (Task.attempt UrlLoaded) (requestUrlById urlId) ]
+    , [ Strict <| Reader.map (Task.attempt UrlLoaded) (requestUrlById urlId)
+      , Strict <| Reader.map (Task.attempt LoadSetting) requestOrganizationSetting
+      ]
     )
 
 
@@ -52,13 +58,21 @@ type Msg
     | UpdateUrl
     | UpdateUrlResponse (Result GQLClient.Error UrlResponse)
     | UrlLoaded (Result GQLClient.Error (Maybe UrlData))
+    | LoadSetting (Result GQLClient.Error Setting)
 
 
 update : Msg -> Model -> ( Model, List (ReaderCmd Msg) )
 update msg model =
     case msg of
         UrlInput url ->
-            ( { model | url = Field.update model.url url }, [] )
+            let
+                newUrl =
+                    unless
+                        (String.startsWith <| Maybe.withDefault "" model.baseUrl)
+                        (always <| Maybe.withDefault "" model.baseUrl)
+                        url
+            in
+            ( { model | url = Field.update model.url newUrl }, [] )
 
         UpdateUrl ->
             let
@@ -105,6 +119,27 @@ update msg model =
 
         UrlLoaded (Err err) ->
             ( { model | errors = [ "There was an error while loading the url" ] }, [] )
+
+        LoadSetting (Ok setting) ->
+            let
+                baseUrl =
+                    Maybe.map
+                        (unless
+                            (String.endsWith "/")
+                            (flip String.append "/")
+                        )
+                        setting.base_url
+            in
+            ( { model
+                | baseUrl = baseUrl
+                , url = Field.update model.url (Maybe.withDefault "" baseUrl)
+                , errors = []
+              }
+            , []
+            )
+
+        LoadSetting (Err err) ->
+            ( { model | errors = [ "Something went wrong while fetching the base Url" ] }, [] )
 
 
 
