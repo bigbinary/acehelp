5a51effcfee66c6d1d4b827e5f166e1a6239503f
Tejaswini <tejaswini776@gmail.com>
2018-10-09 11:24:10 +0530

    updated temporary article cleanup service 1. added destroying article function to service 2. updated schedule for article cleanup

---
+++ app/services/temporary_articles_cleanup_service.rb
@@ -0,0 +1,18 @@
+# frozen_string_literal: true
+
+class TemporaryArticlesCleanupService
+  def initialize
+    @articles = Article.temporary_articles
+  end
+
+  def process
+    article_saved_two_hours_ago.destroy_all
+  end
+
+
+  private
+
+    def article_saved_two_hours_ago
+      @articles.article_saved_two_hours_ago
+    end
+end
--- app/workers/temporary_articles_daily_cleanup_worker.rb
+++ app/workers/temporary_articles_daily_cleanup_worker.rb
@@ -4,13 +4,7 @@ class TemporaryArticlesDailyCleanupWorker
   include Sidekiq::Worker
 
   def perform
-    temporary_articles.destroy_all
+    TemporaryArticlesCleanupService.new.process
     logger.info("Temporary articles deleted successfully.")
   end
-
-  private
-
-    def temporary_articles
-      TemporaryArticleService.new.article_saved_two_hours_ago
-    end
 end
