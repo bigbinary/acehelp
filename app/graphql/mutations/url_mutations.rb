43b848d8adac49ca56190f0e8e9144e8dca90a86
Tejaswini <tejaswini776@gmail.com>
2018-10-09 18:20:10 +0530

    Updated url_mutation 1. If we are not sending any category we are removing url_categories 2. checked the flow found one issue while saving url with `is` rule

--- app/graphql/mutations/url_mutations.rb
+++ app/graphql/mutations/url_mutations.rb
@@ -100,17 +100,21 @@ class Mutations::UrlMutations
     return_field :errors, types[Types::ErrorType]
 
     resolve ->(_obj, inputs, context) {
-      # raise GraphQL::ExecutionError, "Not logged in" unless context[:current_user]
+      raise GraphQL::ExecutionError, "Not logged in" unless context[:current_user]
       url = Url.find_by(id: inputs[:id], organization_id: context[:organization].id)
       if url.nil?
         errors = Utils::ErrorHandler.new.error("Url not found", context)
       else
-        categories = Category.where(id: inputs[:category_ids])
-        if categories.any?
-          url.categories << categories
-          updated_url = url
+        if inputs[:category_ids].empty?
+          url.url_categories.delete_all
         else
-          errors = Utils::ErrorHandler.new.error("Categories not present.", context)
+          categories = Category.where(id: inputs[:category_ids])
+          if categories.any?
+            url.categories << categories
+            updated_url = url
+          else
+            errors = Utils::ErrorHandler.new.error("Categories not present.", context)
+          end
         end
       end
       {
