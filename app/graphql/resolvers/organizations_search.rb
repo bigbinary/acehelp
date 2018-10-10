4e7cb78bb95bef02de5251da660225d652bb67d2
Vishal Telangre <the@vishaltelangre.com>
2018-10-10 19:38:04 +0530

    Set "widget_installed" to true when an initial request is issued by widget
    Investigated about which request is issued by the widget when it renders on the host website.
    Learned that the "request" is passed-down to the GraphQL functions in the "context" hash.
    In the #call method of  the Resolvers::OrganizationsSearch function, inspected the value of the "context[:request].headers['X-Client']" for the requests issued by the widget.
    Added WIDGET_CLIENT_REQUEST_HEADER constant.
    Added logic to fetch the value of the "X-Client" request header from the context hash and update the "widget_installed" field with true value of the setting record associated with the organization matched with the passed-in API key.

--- app/graphql/resolvers/organizations_search.rb
+++ app/graphql/resolvers/organizations_search.rb
@@ -1,9 +1,21 @@
 # frozen_string_literal: true
 
 class Resolvers::OrganizationsSearch < GraphQL::Function
+  WIDGET_CLIENT_REQUEST_HEADER = "widget"
+
   type Types::OrganizationType
 
+
   def call(obj, args, context)
-    Organization.find_by(api_key: context[:organization].api_key)
+    organization = Organization.includes(:setting).find_by(api_key: context[:organization].api_key)
+    request = context[:request]
+    request_headers = request && request.headers
+    client_request_header = request_headers && request_headers["X-Client"]
+
+    if organization && client_request_header == WIDGET_CLIENT_REQUEST_HEADER
+      organization.setting.update_attributes!(widget_installed: true)
+    end
+
+    organization
   end
 end
