c6c08605f70fd5590eaa108ee7e7bf7c4f706820
Vishal Telangre <the@vishaltelangre.com>
2018-10-11 12:58:13 +0530

    Extract out the logic to identify whether the request is issued by client or not
    Added #request_client_is_widget? method in the GraphqlController which checks if the "X-Client" request header has the value "widget" or not.
    Assigned the value of this method to the :request_client_is_widget key in the #context hash.
    
    Removed the similar logic from the #call method of Resolvers::OrganizationsSearch GraphQL function and instead made use of the  : request_client_is_widget key from the context hash to make the decision whether to update the "widget_installed" field in the Setting record or not.

--- app/graphql/resolvers/organizations_search.rb
+++ app/graphql/resolvers/organizations_search.rb
@@ -1,18 +1,13 @@
 # frozen_string_literal: true
 
 class Resolvers::OrganizationsSearch < GraphQL::Function
-  WIDGET_CLIENT_REQUEST_HEADER = "widget"
-
   type Types::OrganizationType
 
 
   def call(obj, args, context)
     organization = Organization.includes(:setting).find_by(api_key: context[:organization].api_key)
-    request = context[:request]
-    request_headers = request && request.headers
-    client_request_header = request_headers && request_headers["X-Client"]
 
-    if organization && client_request_header == WIDGET_CLIENT_REQUEST_HEADER
+    if organization && context[:request_client_is_widget]
       organization.setting.update_attributes!(widget_installed: true)
     end
 
