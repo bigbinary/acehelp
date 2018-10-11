90534831eb4c1a8f26e256fcf769682003dbda5e
Tejaswini <tejaswini776@gmail.com>
2018-10-11 19:11:16 +0530

    Added reference to the redis connection size

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,6 +1,9 @@
 Sidekiq::Extensions.enable_delay!
 
 Sidekiq.configure_server do |config|
+  # Reference:
+  # 1. https://github.com/mperham/sidekiq/wiki/Using-Redis#complete-control
+  # 2. https://elements.heroku.com/addons/redistogo, Max connection 10
   config.redis = { url: ENV['REDIS_URL'], size: 9 }
   schedule_file = Rails.application.secrets.sidekiq_schedule_file
 
