e5e80470e1122688e1e86026538630eb033a4af7
Vishal Telangre <the@vishaltelangre.com>
2018-10-11 16:04:26 +0530

    Add tests
    Added a test "Sets 'widget_installed' to 'true' when 'X-Client' request header has 'widget' value" for the GraphqlController.
    
    Also, added another test "Does nothing when 'X-Client' request header does not have 'widget' value" for the GraphqlController.

--- test/controllers/graphql_execution/base_controller_test.rb
+++ test/controllers/graphql_execution/base_controller_test.rb
@@ -34,4 +34,52 @@ class GraphqlExecution::BaseControllerTest < ActionDispatch::IntegrationTest
       result = AceHelp::Client.execute(query)
     end
   end
+
+  test "Sets 'widget_installed' to 'true' when 'X-Client' request header has 'widget' value" do
+    assert_not @organization.reload.setting.widget_installed
+
+    post "/graphql",
+         params: { query: "query { organizations { id name widget_visibility }}" },
+         headers: { "api-key": @organization.api_key, "X-Client": "widget" }
+
+    expected_response = {
+      "data" => {
+        "organizations" => {
+          "id" => @organization.id,
+          "name" => @organization.name,
+          "widget_visibility" => @organization.setting.enable?
+        }
+      }
+    }
+
+    assert_response :success
+
+    assert_equal(expected_response, response.parsed_body)
+
+    assert @organization.reload.setting.widget_installed
+  end
+
+  test "Does nothing when 'X-Client' request header does not have 'widget' value" do
+    assert_not @organization.reload.setting.widget_installed
+
+    post "/graphql",
+         params: { query: "query { organizations { id name widget_visibility }}" },
+         headers: { "api-key": @organization.api_key, "X-Client": "dashboard" }
+
+    expected_response = {
+      "data" => {
+        "organizations" => {
+          "id" => @organization.id,
+          "name" => @organization.name,
+          "widget_visibility" => @organization.setting.enable?
+        }
+      }
+    }
+
+    assert_response :success
+
+    assert_equal(expected_response, response.parsed_body)
+
+    assert_not @organization.reload.setting.widget_installed
+  end
 end
