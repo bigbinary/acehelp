94fc3b547dd1602486ca4c7582c7dc83d33129a0
Vishal Telangre <the@vishaltelangre.com>
2018-10-12 19:02:29 +0530

    Add rails-erb-loader
    Because passing down backend variables to the JS code using "Webpacker::Compile.env" wasn't working as expected, thought of pre-compiling the "javascript/packs/client.js" script using Erb compiler first by adding the ".js.erb" extension to it.
    
    Learned that we need to add the "rails-erb-loader" to add Erb support to the JS templates - https://github.com/rails/webpacker#erb.
    
    Installed "rails-erb-loader" NPM package and added bunch of other required configuration using the "bundle exec rails webpacker:install:erb" command.

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,5 +1,7 @@
-const { environment } = require('@rails/webpacker')
-const elm =  require('./loaders/elm')
+const { environment } = require("@rails/webpacker");
+const erb = require("./loaders/erb");
+const elm = require("./loaders/elm");
 
-environment.loaders.append('elm', elm)
-module.exports = environment
+environment.loaders.append("elm", elm);
+environment.loaders.append("erb", erb);
+module.exports = environment;
